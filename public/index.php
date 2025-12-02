6037464b846ee0f6459e068c83dacebbe190cffd
IrinaZ1990 <irin.s.fedorova@gmail.com>
2025-12-02 20:09:46 +0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use morphos\Russian;
+
+header('Content-Type: text/plain; charset=utf-8');
+
+$name = $_GET['name'] ?? '';
+$case = $_GET['case'] ?? 'именительный';
+
+if (!$name) {
+    http_response_code(400);
+    echo "Missing 'name' parameter";
+    exit;
+}
+
+echo Russian\inflectName($name, $case);
